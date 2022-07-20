28afef40078dc88b50abbd11c3f9b476f3cb998d
Fabian Portilla <fas_456@hotmail.com>
2022-07-20 10:47:11 -0500

    Add login request handler

---
+++ api/controllers/login.js
@@ -0,0 +1,42 @@
+const loginRouter = require('express').Router()
+const bcrypt = require('bcryptjs')
+const User = require('../models/users')
+const jwt = require('jsonwebtoken')
+const config = require('../utils/config')
+
+loginRouter.post('/', async (request, response) => {
+
+  const { username, password, rol } = request.body
+
+  const user = await User.findOne({ username })
+
+  const passwordCorrect = user === null
+    ? false
+    : await bcrypt.compare(password, user.passwordHash ? user.passwordHash : '')
+
+  if (!rol) {
+    return response.status(401).json({
+      error: 'Please select a rol'
+    })
+  }
+
+  if (!(user && passwordCorrect)) {
+
+    return response.status(401).json({
+      error: 'invalid username or password'
+    })
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user.id
+  }
+
+  const token = jwt.sign(userForToken, `${config.SECRET}`)
+
+  return response
+    .status(200)
+    .send({ token, username: user.username, name: user.name })
+})
+
+module.exports = loginRouter
