c6e461ab45f7cce432f083942c8c11d7cecdd17d
Fabian Portilla <fas_456@hotmail.com>
2022-07-21 11:19:41 -0500

    Refactor modules component

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -5,41 +5,61 @@ import { FcDiploma2 } from 'react-icons/fc';
 import { GiStoneAxe } from 'react-icons/gi';
 import { MdOutlineForum } from 'react-icons/md';
 
+const resourcesButtons = [
+  {
+    id: 1,
+    onclick: null,
+    customClass: 'survey',
+    type: 'button',
+    children: {
+      component: <RiSurveyFill />,
+      text: 'Survey'
+    }
+  },
+  {
+    id: 2,
+    onclick: null,
+    customClass: 'certificate',
+    type: 'button',
+    children: {
+      component: <FcDiploma2 />,
+      text: 'certificate'
+    }
+  },
+  {
+    id: 3,
+    onclick: null,
+    customClass: 'test',
+    type: 'button',
+    children: {
+      component: <GiStoneAxe />,
+      text: 'test'
+    }
+  },
+  {
+    id: 4,
+    onclick: null,
+    customClass: 'forum',
+    type: 'button',
+    children: {
+      component: <MdOutlineForum />,
+      text: 'forum'
+    }
+  }
+]
+
 const Resources = () => {
 
   return (
     <div className='resources'>
-      <Button
-        onClick={null}
-        customClass={'survey'}
-        type={'button'}
-      >
-        <RiSurveyFill /> Survey
-      </Button >
-
-      <Button
-        onClick={null}
-        customClass={'certificate'}
-        type={'button'}
-      >
-        <FcDiploma2 /> certificate
-      </Button >
-
-      <Button
-        onClick={null}
-        customClass={'test'}
-        type={'button'}
-      >
-        <GiStoneAxe /> test
-      </Button >
-
-      <Button
-        onClick={null}
-        customClass={'forum'}
-        type={'button'}
-      >
-        <MdOutlineForum /> forum
-      </Button >
+      {resourcesButtons.map(resource => {
+        return <Button
+          key={resource.id}
+          onClick={resource.onclick}
+          customClass={resource.customClass}
+          type={resource.type}
+        > {resource.children.component} {resource.children.text}</Button>
+      })}
     </div>
   )
 }
