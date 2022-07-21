2fb60bc2a7743d6b0d00a55edd03e717b7d9f50f
Fabian Portilla <fas_456@hotmail.com>
2022-07-21 10:24:53 -0500

    Update student component to have a description component

--- src/components/Student.js
+++ src/components/Student.js
@@ -2,6 +2,10 @@ import React from 'react'
 import '../style/student.css'
 import courseImg from '../assets/curso-bio-basico.png'
 import Resources from './Resources'
+import Indicators from './Indicators'
+
+const description = "This course contains the biosecurity measures implemente by this random generic company, along with regulations issued by the national goverment on the ocassion of the sanitary emergency that is facing the country and the world.You will find not only COVID - 19 general information, overview and self - caring recommendations but also against the return for the development of face - to - face activities in each of the workplaces."
+
 const Student = () => {
   return (
     <section className='student_section'>
@@ -12,7 +16,14 @@ const Student = () => {
         </div>
         <Resources />
       </div>
-      <div className='description'>description</div>
+      <div className='description'>
+        <div className='text'>
+
+          {description}
+        </div>
+
+        <Indicators />
+      </div>
       <div className='modules'>modules</div>
     </section>
   )
