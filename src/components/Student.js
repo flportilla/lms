6b422adbb9c4d788d88e6af626a58557c889ead5
Fabian Portilla <fas_456@hotmail.com>
2022-07-21 11:19:17 -0500

    Refactor Student component

--- src/components/Student.js
+++ src/components/Student.js
@@ -3,8 +3,32 @@ import '../style/student.css'
 import courseImg from '../assets/curso-bio-basico.png'
 import Resources from './Resources'
 import Indicators from './Indicators'
+import Module from './Module'
 
 const description = "This course contains the biosecurity measures implemente by this random generic company, along with regulations issued by the national goverment on the ocassion of the sanitary emergency that is facing the country and the world.You will find not only COVID - 19 general information, overview and self - caring recommendations but also against the return for the development of face - to - face activities in each of the workplaces."
+const modulesInfo = [
+  {
+    id: 1,
+    moduleNumber: 'Módulo 1',
+    topic: 'General information'
+  },
+  {
+    id: 2,
+    moduleNumber: 'Módulo 2',
+    topic: 'On daily activities'
+  },
+  {
+    id: 3,
+    moduleNumber: 'Módulo 3',
+    topic: 'On specific activities'
+  },
+  {
+    id: 4,
+    moduleNumber: 'Módulo 4',
+    topic: 'Responsibilities'
+  }
+
+]
 
 const Student = () => {
   return (
@@ -24,7 +48,15 @@ const Student = () => {
 
         <Indicators />
       </div>
-      <div className='modules'>modules</div>
+      <div className='modules'>
+        {modulesInfo.map(module => {
+          return <Module
+            key={module.id}
+            title={module.moduleNumber}
+            topic={module.topic}
+          />
+        })}
+      </div>
     </section>
   )
 }
