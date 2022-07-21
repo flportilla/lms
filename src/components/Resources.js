eb28fbc4092b347b5d382f82e749c62a5c222ecd
Fabian Portilla <fas_456@hotmail.com>
2022-07-21 09:49:49 -0500

    Refactor students and create Resources component

---
+++ src/components/Resources.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import Button from './Button'
+import { RiSurveyFill } from 'react-icons/ri';
+import { FcDiploma2 } from 'react-icons/fc';
+import { GiStoneAxe } from 'react-icons/gi';
+import { MdOutlineForum } from 'react-icons/md';
+
+
+
+const resources = () => {
+
+  const survey = <><div> survey </div></>
+
+  return (
+    <div className='resources'>
+      <Button
+        onClick={null}
+        customClass={'survey'}
+        type={'button'}
+      >
+        <RiSurveyFill /> Survey
+      </Button >
+
+      <Button
+        onClick={null}
+        customClass={'certificate'}
+        type={'button'}
+      >
+        <FcDiploma2 /> certificate
+      </Button >
+
+      <Button
+        onClick={null}
+        customClass={'test'}
+        type={'button'}
+      >
+        <GiStoneAxe /> test
+      </Button >
+
+      <Button
+        onClick={null}
+        customClass={'forum'}
+        type={'button'}
+      >
+        <MdOutlineForum /> forum
+      </Button >
+    </div>
+  )
+}
+
+export default resources
