ab79490d7b53adfa5ae545d8835a6cdd0d5217bb
Fabian Portilla <fas_456@hotmail.com>
2022-07-21 09:48:18 -0500

    Add student component and styles

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import '../style/student.css'
+import courseImg from '../assets/curso-bio-basico.png'
+import Resources from './Resources'
 const Student = () => {
   return (
-    <h1>Student</h1>
+    <section className='student_section'>
+
+      <div className='course_name'>
+        <div className='course_name_img_container'>
+          <img src={courseImg} alt="course name" />
+        </div>
+        <Resources />
+      </div>
+      <div className='description'>description</div>
+      <div className='modules'>modules</div>
+    </section>
   )
 }
 
