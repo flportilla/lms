7a9ccc11e4dca9fcb9c7a541f293d12bbfe0ad51
Fabian Portilla <fas_456@hotmail.com>
2022-07-27 09:05:08 -0500

    Show a list of the test created

--- src/components/ListTests.js
+++ src/components/ListTests.js
@@ -1,20 +1,51 @@
 import React, { useEffect, useState } from 'react'
 import testHelper from '../services/test'
+import '../style/testList.css'
 
 const ListTests = ({ rol }) => {
 
-  const [tests, setTests] = useState()
+  const [tests, setTests] = useState([])
+  const [showQuestions, setShowQuestions] = useState(false)
 
   useEffect(() => {
 
+    const token = JSON.parse(window.localStorage.getItem('token'))
+
+    testHelper.setToken(token)
+
     testHelper.listTests()
-      .then(test => setTests)
+      .then(test => setTests([...test]))
 
   }, [])
-
-
   return (
-    <div>{tests}</div>
+    <div className='test_container'>
+      <button
+        type='button'
+        onClick={() => { setShowQuestions(!showQuestions) }}
+      >Show questions</button>
+      {
+        tests.map(test => {
+          return (
+            <div key={test.id}>
+              <h2>{test.name}</h2>
+              <div>
+                {
+                  showQuestions
+                    ? <div className='questions_container'>
+                      <ul>
+                        {test.questions.map(question => <li key={question.id}>{question.statement}</li>)}
+                      </ul>
+                    </div>
+                    : null
+                }
+              </div>
+
+            </div>
+          )
+        })
+
+      }
+    </div>
   )
 }
 
