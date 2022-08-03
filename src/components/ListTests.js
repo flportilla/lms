949a38f131ae87685a818ec100b1b901ce6eb68f
Fabian Portilla <fas_456@hotmail.com>
2022-08-03 13:47:04 -0500

    Add loading state to list test/questions

--- src/components/ListTests.js
+++ src/components/ListTests.js
@@ -3,108 +3,119 @@ import testHelper from '../services/test'
 import '../style/testList.css'
 import Button from './Button'
 import { useNavigate } from 'react-router-dom'
+import Loading from './Loading'
 
-const ListTests = ({ rol }) => {
+const ListTests = ({ rol, isLoading, loadingDispatch }) => {
 
   const [tests, setTests] = useState([])
-
+  const [loadInfo, setloadInfo] = useState(false)
   const [showQuestions, setShowQuestions] = useState(false)
   const navigate = useNavigate()
 
   useEffect(() => {
 
     const token = JSON.parse(window.localStorage.getItem('token'))
+    if (rol === 'Professor') {
+      setloadInfo(true)
+      testHelper.setToken(token)
 
-    testHelper.setToken(token)
+      testHelper.listTests()
+        .then(test => setTests([...test]))
+        .then(res => setloadInfo(false))
+    }
 
-    testHelper.listTests()
-      .then(test => setTests([...test]))
-
-  }, [])
+  }, [isLoading])
 
   const handleDeleteRequest = async (id, testName) => {
 
     const result = window.confirm(`Are you sure you want to delete ${testName}?`);
 
-    try {
-      if (result) {
-        await testHelper.removeTest(id)
-        window.location.reload()
-      }
-
-    } catch (error) {
-      console.error(error)
+    if (result) {
+      loadingDispatch({ type: 'loading' })
+      await testHelper.removeTest(id)
+      loadingDispatch({ type: 'notLoading' })
     }
+
   }
 
   const handleSelection = async (target, id) => {
     const updatedStatusTest = { ...tests, selectedTest: target.checked }
 
+
     const token = JSON.parse(window.localStorage.getItem('token'))
     testHelper.setToken(token)
-
+    loadingDispatch({ type: 'loading' })
     await testHelper.updateTest(id, updatedStatusTest)
+    loadingDispatch({ type: 'notLoading' })
+
   }
 
   return (
     <div className='test_container'>
-      <Button
-        type='button'
-        onClick={() => { setShowQuestions(!showQuestions) }}
-        customClass={'show_questions'}
-      >
-        Show questions
-      </Button>
-      <h3 style={{ marginBottom: '15px' }}
-      >
-        Select a test and save to make it available for students</h3>
       {
-        tests.map((test, index) => {
-          return (
-            <div key={test.id}>
-              <div style={{ display: 'flex', margin: '0 10px 0 0' }}>
+        loadInfo
+          ? <Loading />
+          : <>
+            <Button
+              type='button'
+              onClick={() => { setShowQuestions(!showQuestions) }}
+              customClass={'show_questions'}
+            >
+              Show questions
+            </Button>
+            <h3 style={{ marginBottom: '15px' }}
+            >
+              Select a test and save to make it available for students</h3>
+            {
+              tests.map((test, index) => {
+                return (
+                  <div key={test.id}>
+                    <div style={{ display: 'flex', margin: '0 10px 0 0' }}>
 
-                <label style={{ display: 'flex' }} htmlFor={`${index}`}>
-                  <input
-                    id={`${index}`}
-                    type={'checkbox'}
-                    defaultChecked={test.selectedTest ? test.selectedTest : false}
-                    onChange={({ target }) => handleSelection(target, test.id)}
-                  />
+                      <label style={{ display: 'flex' }} htmlFor={`${index}`}>
+                        <input
+                          id={`${index}`}
+                          type={'checkbox'}
+                          defaultChecked={test.selectedTest ? test.selectedTest : false}
+                          onChange={({ target }) => handleSelection(target, test.id)}
+                        />
 
-                  <h2>{test.name}</h2>
-                </label>
+                        <h2>{test.name}</h2>
+                      </label>
 
-                <Button
-                  type={'button'}
-                  onClick={() => handleDeleteRequest(test.id, test.name)}
-                  customClass={'delete_test'}
-                >
-                  Delete
-                </Button>
-              </div>
-              <div>
-                {
-                  showQuestions
-                    ? <div className='questions_container'>
-                      <ul>
-                        {test.questions.map(question => <li key={question.id}>{question.statement}</li>)}
-                      </ul>
+                      <Button
+                        type={'button'}
+                        onClick={() => handleDeleteRequest(test.id, test.name)}
+                        customClass={'delete_test'}
+                      >
+                        Delete
+                      </Button>
                     </div>
-                    : null
-                }
-              </div>
-            </div>
-          )
-        })
+                    <div>
+                      {
+                        showQuestions
+                          ? <div className='questions_container'>
+                            <ul>
+                              {test.questions.map(question => <li key={question.id}>{question.statement}</li>)}
+                            </ul>
+                          </div>
+                          : null
+                      }
+                    </div>
+                  </div>
+                )
+              })
+            }
+            <Button
+              type='button'
+              onClick={() => { navigate('/Professor'); }}
+              customClass={'show_questions'}
+            >
+              Save
+            </Button>
+          </>
       }
-      <Button
-        type='button'
-        onClick={() => { navigate('/Professor'); window.location.reload() }}
-        customClass={'show_questions'}
-      >
-        Save
-      </Button>
+
     </div>
   )
 }
