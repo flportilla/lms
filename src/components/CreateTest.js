05a83a64e19810e0c77ba00f6c51678b0671af91
Fabian Portilla <fas_456@hotmail.com>
2022-08-03 13:46:39 -0500

    Add loading state to create test component

--- src/components/CreateTest.js
+++ src/components/CreateTest.js
@@ -1,10 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from './Button'
 import '../style/testCreator.css'
 import testHelper from '../services/test'
 import { useNavigate } from 'react-router-dom'
+import questionHelper from '../services/questions'
+import Loading from './Loading'
 
-const CreateTest = ({ questionsList }) => {
+const CreateTest = ({ isLoading, loadingDispatch }) => {
+
+  const [questionsList, setQuestionsList] = useState([])
+  const [loadInfo, setloadInfo] = useState(false)
+  const isLogged = window.localStorage.getItem('rol') === 'Professor'
+
+
+  useEffect(() => {
+
+    const token = JSON.parse(window.localStorage.getItem('token'))
+    questionHelper.setToken(token)
+
+    if (isLogged) {
+      setloadInfo(true)
+
+      questionHelper.listQuestions()
+        .then(question => setQuestionsList(question))
+        .then(res => setloadInfo(false))
+
+    }
+
+  }, [isLoading])
 
   const [testName, setTestName] = useState('')
   const navigate = useNavigate()
@@ -23,18 +46,17 @@ const CreateTest = ({ questionsList }) => {
       questions: questionsSelected
     }
 
-    try {
-      testHelper.setToken(JSON.parse(window.localStorage.getItem('token')))
-      await testHelper.addTest(exam)
+    testHelper.setToken(JSON.parse(window.localStorage.getItem('token')))
 
-      alert('Test created')
-      navigate('/Professor')
+    loadingDispatch({ type: 'loading' })
+    await testHelper.addTest(exam)
+    loadingDispatch({ type: 'notLoading' })
 
-      questionsList.map(question => question.selected = false)
+    alert('Test created')
+    navigate('/Professor')
 
-    } catch (error) {
-      console.error(error)
-    }
+    questionsList.map(question => question.selected = false)
+
   }
 
   const handleSelection = (target, id) => {
@@ -43,46 +65,54 @@ const CreateTest = ({ questionsList }) => {
   }
 
   return (
-    <form
-      className='create_test_form'
-    >
-      <input
-        placeholder='...Enter test name here...'
-        className='test_name_input'
-        required
-        type={'text'}
-        value={testName}
-        onChange={({ target }) => setTestName(target.value)}
-      />
-      <p>Please select the questions that you would like to add to this examn</p>
-      {
-        questionsList.map((question, index) => {
-          return (
-            <div
-              className='question_container'
 
-              key={question.id}
-            >
-              <label htmlFor={index + 1}
-              >
-                <input
-                  id={index + 1}
-                  type={'checkbox'}
-                  onChange={({ target }) => handleSelection(target, question.id)}
-                />
-                {index + 1}. {question.statement}
-              </label>
-            </div>
-          )
-        })
+    <>
+      {
+        loadInfo
+          ? <Loading />
+          : <form
+            className='create_test_form'
+          >
+            <input
+              placeholder='...Enter test name here...'
+              className='test_name_input'
+              required
+              type={'text'}
+              value={testName}
+              onChange={({ target }) => setTestName(target.value)}
+            />
+            <p>Please select the questions that you would like to add to this examn</p>
+            {
+              questionsList.map((question, index) => {
+                return (
+                  <div
+                    className='question_container'
+
+                    key={question.id}
+                  >
+                    <label htmlFor={index + 1}
+                    >
+                      <input
+                        id={index + 1}
+                        type={'checkbox'}
+                        onChange={({ target }) => handleSelection(target, question.id)}
+                      />
+                      {index + 1}. {question.statement}
+                    </label>
+                  </div>
+                )
+              })
+            }
+            <Button
+              onClick={handleTestCreation}
+              children={'Create'}
+              customClass={'create_test'}
+              type={'submit'}
+            />
+          </form>
+
       }
-      <Button
-        onClick={handleTestCreation}
-        children={'Create'}
-        customClass={'create_test'}
-        type={'submit'}
-      />
-    </form>
+    </>
   )
 }
 
