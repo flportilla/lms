1f807bc7c2a14145667d1f6638d691bbe55eb580
Fabian Portilla <fas_456@hotmail.com>
2022-08-05 16:08:33 -0500

    Refactor question form to use less states

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -5,39 +5,96 @@ import Button from './Button'
 import TextArea from './TextArea'
 import { useNavigate, useLocation } from 'react-router-dom';
 
-const QuestionForm = ({
-  loadingDispatch
-}) => {
 
-  const { state } = useLocation()
-  const { request, updatedQuestion: autoFilledQuestion } = state
-  const isLogged = window.localStorage.getItem('role') === 'Professor'
 
-  const [statement, setStatement] = useState(request
-    ? `${autoFilledQuestion.statement}`
-    : 'statement'
-  )
-  const [option1, setOption1] = useState(request
-    ? `${autoFilledQuestion.option1}`
-    : 'option1'
-  )
-  const [option2, setOption2] = useState(request
-    ? `${autoFilledQuestion.option2}`
-    : 'option2'
-  )
-  const [option3, setOption3] = useState(request
-    ? `${autoFilledQuestion.option3}`
-    : 'option3'
-  )
-  const [option4, setOption4] = useState(request
-    ? `${autoFilledQuestion.option4}`
-    : 'option4'
-  )
-  const [answer, setAnswer] = useState(request
-    ? `${autoFilledQuestion.answer}`
-    : 'answer'
-  )
+
+
+// const questionsForm = [
+//   {
+//     id: 1,
+//     htmlFor: 'statement',
+//     customClass: 'textarea',
+//     type: 'text',
+//     isRequired: true,
+//     children: 'statement',
+//     value: newQuestion.statement,
+//     onChange: setNewQuestion({ ...newQuestion, statement: newQuestion.statement })
+
+//   },
+//   {
+//     id: 2,
+//     htmlFor: 'option1',
+//     customClass: 'textarea',
+//     type: 'text',
+//     isRequired: true,
+//     children: 'Option 1',
+//     value: newQuestion.opt1,
+//     onChange: setNewQuestion({ ...newQuestion, opt1: newQuestion.opt1 })
+//   },
+//   {
+//     id: 3,
+//     htmlFor: 'option2',
+//     customClass: 'textarea',
+//     type: 'text',
+//     isRequired: true,
+//     children: 'Option 2',
+//     value: newQuestion.opt2,
+//     onChange: setNewQuestion({ ...newQuestion, opt2: newQuestion.opt2 })
+//   },
+//   {
+//     id: 4,
+//     htmlFor: 'option3',
+//     customClass: 'textarea',
+//     type: 'text',
+//     isRequired: true,
+//     children: 'Option 3',
+//     value: newQuestion.opt3,
+//     onChange: setNewQuestion({ ...newQuestion, opt3: newQuestion.opt3 })
+//   },
+//   {
+//     id: 5,
+//     htmlFor: 'option4',
+//     customClass: 'textarea',
+//     type: 'text',
+//     isRequired: true,
+//     children: 'Option 4',
+//     value: newQuestion.opt4,
+//     onChange: ({ target }) => setNewQuestion({ ...newQuestion, opt4: target.value })
+//   },
+//   {
+//     id: 6,
+//     htmlFor: 'answer',
+//     customClass: 'textarea',
+//     type: 'text',
+//     isRequired: true,
+//     children: 'Answer',
+//     value: answer,
+//     onChange: setAnswer
+//   },
+// ]
+
+
+const QuestionForm = ({ loadingDispatch }) => {
+
   const navigate = useNavigate()
+  const { state } = useLocation()
+
+
+  const { updateRequest, updatedQuestion: autoFilledQuestion, role } = state
+
+  const [testnewQuestion, testsetNewQuestion] = useState({
+    statement: updateRequest ? autoFilledQuestion.statement : '',
+    opt1: updateRequest ? autoFilledQuestion.option1 : '',
+    opt2: updateRequest ? autoFilledQuestion.option2 : '',
+    opt3: updateRequest ? autoFilledQuestion.option3 : '',
+    opt4: updateRequest ? autoFilledQuestion.option4 : '',
+  })
+
+  const [testanswer, testsetAnswer] = useState({
+    answer: updateRequest ? autoFilledQuestion.answer : ''
+  })
+
+  console.log(testnewQuestion, testanswer)
 
   const questionsForm = [
     {
@@ -47,8 +104,8 @@ const QuestionForm = ({
       type: 'text',
       isRequired: true,
       children: 'statement',
-      value: statement,
-      onChange: setStatement
+      value: testnewQuestion.statement,
+      onChange: (value) => testsetNewQuestion({ ...testnewQuestion, statement: value })
     },
     {
       id: 2,
@@ -57,8 +114,8 @@ const QuestionForm = ({
       type: 'text',
       isRequired: true,
       children: 'Option 1',
-      value: option1,
-      onChange: setOption1
+      value: testnewQuestion.opt1,
+      onChange: (value) => testsetNewQuestion({ ...testnewQuestion, opt1: value })
     },
     {
       id: 3,
@@ -67,8 +124,8 @@ const QuestionForm = ({
       type: 'text',
       isRequired: true,
       children: 'Option 2',
-      value: option2,
-      onChange: setOption2
+      value: testnewQuestion.opt2,
+      onChange: (value) => testsetNewQuestion({ ...testnewQuestion, opt2: value })
     },
     {
       id: 4,
@@ -77,8 +134,8 @@ const QuestionForm = ({
       type: 'text',
       isRequired: true,
       children: 'Option 3',
-      value: option3,
-      onChange: setOption3
+      value: testnewQuestion.opt3,
+      onChange: (value) => testsetNewQuestion({ ...testnewQuestion, opt3: value })
     },
     {
       id: 5,
@@ -87,86 +144,86 @@ const QuestionForm = ({
       type: 'text',
       isRequired: true,
       children: 'Option 4',
-      value: option4,
-      onChange: setOption4
+      value: testnewQuestion.opt4,
+      onChange: (value) => testsetNewQuestion({ ...testnewQuestion, opt4: value })
     },
-    {
-      id: 6,
-      htmlFor: 'answer',
-      customClass: 'textarea',
-      type: 'text',
-      isRequired: true,
-      children: 'Answer',
-      value: answer,
-      onChange: setAnswer
-    },
+    // {
+    //   id: 6,
+    //   htmlFor: 'answer',
+    //   customClass: 'textarea',
+    //   type: 'text',
+    //   isRequired: true,
+    //   children: 'Answer',
+    //   value: testanswer.answer,
+    //   onChange: (value) => testsetAnswer(value)
+    // },
   ]
 
   //Handles the creation of new questions
   const createQuestionRequest = async (e) => {
 
-    loadingDispatch({ type: 'loading' })
-    try {
+    // loadingDispatch({ type: 'loading' })
+    // try {
 
-      const newQuestion = {
-        statement,
-        option1,
-        option2,
-        option3,
-        option4,
-        answer,
-        loadingDispatch
-      }
+    //   const newQuestion = {
+    //     statement,
+    //     option1,
+    //     option2,
+    //     option3,
+    //     option4,
+    //     answer,
+    //     loadingDispatch
+    //   }
 
-      const token = JSON.parse(window.localStorage.getItem('token'))
-      questionHelper.setToken(token)
+    //   const token = JSON.parse(window.localStorage.getItem('token'))
+    //   questionHelper.setToken(token)
 
-      await questionHelper.addQuestion(newQuestion)
-      loadingDispatch({ type: 'notLoading' })
+    //   await questionHelper.addQuestion(newQuestion)
+    //   loadingDispatch({ type: 'notLoading' })
 
-      alert('Question added')
-    } catch (error) {
-      loadingDispatch({ type: 'notLoading' })
-    }
+    //   alert('Question added')
+    // } catch (error) {
+    //   loadingDispatch({ type: 'notLoading' })
+    // }
 
   }
 
   const updateQuestionRequest = async (e) => {
 
-    loadingDispatch({ type: 'loading' })
-    const { id } = autoFilledQuestion
+    // loadingDispatch({ type: 'loading' })
+    // const { id } = autoFilledQuestion
 
-    const updatedQuestion = {
-      statement,
-      option1,
-      option2,
-      option3,
-      option4,
-      answer
-    }
+    // const updatedQuestion = {
+    //   statement,
+    //   option1,
+    //   option2,
+    //   option3,
+    //   option4,
+    //   answer
+    // }
 
-    try {
+    // try {
 
-      const token = JSON.parse(window.localStorage.getItem('token'))
-      questionHelper.setToken(token)
+    //   const token = JSON.parse(window.localStorage.getItem('token'))
+    //   questionHelper.setToken(token)
 
-      loadingDispatch({ type: 'loading' })
-      await questionHelper.updateQuestion(id, updatedQuestion)
-      loadingDispatch({ type: 'notLoading' })
+    //   loadingDispatch({ type: 'loading' })
+    //   await questionHelper.updateQuestion(id, updatedQuestion)
+    //   loadingDispatch({ type: 'notLoading' })
 
-      alert('Question updated')
+    //   alert('Question updated')
 
-      navigate('/list-questions')
+    //   navigate('/list-questions')
 
-    } catch (error) {
-      loadingDispatch({ type: 'notLoading' })
-    }
+    // } catch (error) {
+    //   loadingDispatch({ type: 'notLoading' })
+    // }
 
   }
 
   return (
     <>{
-      isLogged
+      role === 'PROFESSOR_ROLE'
         ? <form
           className='add_question_form'>
           {
@@ -196,16 +253,16 @@ const QuestionForm = ({
           {
             <>
               {
-                request
+                updateRequest
                   ? <Button
-                    onClick={state.request ? updateQuestionRequest : createQuestionRequest}
+                    onClick={updateRequest ? updateQuestionRequest : createQuestionRequest}
                     children={'Update'}
                     customClass={null}
                     type={'button'}
                   />
                   :
                   <Button
-                    onClick={state.request ? updateQuestionRequest : createQuestionRequest}
+                    onClick={updateRequest ? updateQuestionRequest : createQuestionRequest}
                     children={'Create'}
                     customClass={null}
                     type={'button'}
